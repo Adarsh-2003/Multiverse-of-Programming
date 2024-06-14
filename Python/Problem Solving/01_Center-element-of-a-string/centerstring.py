5e9b3d643f6c8548b14efbd0cd2f849f9b06ef5e
Adarsh-2003 <adarshgupta.contact@gmail.com>
2024-06-14 18:46:35 +0530

    file-structured

--- Python/Problem Solving/01_Center-element-of-a-string/centerstring.py
+++ Python/Problem Solving/01_Center-element-of-a-string/centerstring.py
@@ -4,5 +4,5 @@ s = int(len(a))
 if(s%2 == 0):
     print("Cannot find center element of even string")
 else:
-    k = int(s/2 -0.5)
+    k = int(s/2 -0.5) # k = 9/2 = 4.5 - 0.5 = 4
     print(a[k])
--- Python/Problem Solving/05_Arrays/array01.py
+++ Python/Problem Solving/05_Arrays/array01.py
@@ -20,6 +20,7 @@ Output: 1
 Explanation: Here Arr[1] = 1 exists.
 Your Task:  
 You don't need to read input or print anything. Your task is to complete the function valueEqualToIndex() which takes the array of integers arr[] and n as parameters and returns an array of indices where the given conditions are satisfied. When there is no such element exists then return an empty array of length 0. For C users, you need to modify the array(arr), in place such that the indices not in the final answer should be marked 0.
+
 """
 arr = [11,2,22,4]
 n= 4
