639fd3e99aaae14cc7aa7bbf181e6808c2cee373
Adarsh-2003 <adarshgupta.contact@gmail.com>
2024-08-07 12:02:59 +0530

    insertion-sort

---
+++ Python/Problem Solving/06_Armstrong-number/armstrong.py
@@ -0,0 +1,5 @@
+# 153 is a armstrong number
+# (1 x 1 x 1) + (5 x 5 x 5) + (3 x 3 x 3) = 153
+
+def armstrong(num):
+    
---
+++ Python/Problem Solving/08_Selection-Sort/selectionsort.py
@@ -0,0 +1,26 @@
+# s1 : get the first element of the arr and swap it with the min element of the arr
+# s2 : lower the size of the an array (exclude 1st element)
+# s3 : repeat s1 for the new arr
+# s4 : lower the size of the an array (exclude 1st & 2nd element)
+# repeat until 2nd last element
+
+arr = [ 89, 23, 56, 78, 36, 35 ]
+print(arr)
+arr_l= len(arr)
+
+for i in range (arr_l):
+    mini = i
+    for j in range (i, arr_l):
+        # getting the minimum element
+        if(arr[j] < arr[mini]):
+            mini = j
+            
+    # swapping the first and the min element
+    temp = arr[i]
+    arr[i] = arr[mini]
+    arr[mini] = temp
+    
+print(arr)
+    
+# try to add i+1 in the inner j loop
+# increases efficiency and comparing the first element with itself is unncessary and absurd
