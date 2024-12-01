72fd8cbbe5272d5369fdf36ffcd09c33cace491f
Adarsh-2003 <adarshgupta.contact@gmail.com>
2024-12-02 01:22:28 +0530

    update-file structure

--- Python/File Handling/02_write_file.py
+++
@@ -1,7 +0,0 @@
-with open("File Handling\poem.txt",'r') as f:
-    f_out = open("File Handling\output.txt", 'w')
-    for line in f:
-        token = line.split(" ")
-        wordCount = str(len(token))
-        f_out.write(line + "WordCount : " + wordCount + " ")
-    f_out.close()
--- Python/Problem Solving/01_Center-element-of-a-string/centerstring.py
+++
@@ -1,8 +0,0 @@
-# Find center element of a string; returm "Cannot find center element of even string" if string length is even number
-a ="spiderman"
-s = int(len(a))
-if(s%2 == 0):
-    print("Cannot find center element of even string")
-else:
-    k = int(s/2 -0.5) # k = 9/2 = 4.5 - 0.5 = 4
-    print(a[k])
